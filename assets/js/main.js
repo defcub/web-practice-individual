4dbe222c45e2a347526f1f379d9dfd5dcc67cc81
Taras Kaban <vasylua6@gmail.com>
2026-06-13 00:39:56 +0300

    Add JS logic to portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,32 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll('.animate-on-scroll');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// =========================================
+// PORTFOLIO SLIDER
+// =========================================
+
+const track = document.getElementById('portfolioTrack');
+const slides = document.querySelectorAll('.portfolio-slide');
+const btnPrev = document.getElementById('btnPrev');
+const btnNext = document.getElementById('btnNext');
+const dots = document.querySelectorAll('.portfolio-pagination .dot');
+
+let currentIndex = 0;
+
+function goToSlide(index) {
+    currentIndex = index;
+
+    const gap = parseInt(window.getComputedStyle(track).gap) || 0;
+    track.style.transform = `translateX(-${index * (slides[0].clientWidth + gap)}px)`;
+    dots.forEach((dot, i) => dot.classList.toggle('active', i === index));
+
+    btnPrev.style.opacity = index === 0 ? "0.5" : "1";
+    btnNext.style.opacity = index === slides.length - 1 ? "0.5" : "1";
+}
+btnNext.addEventListener('click', () => { if (currentIndex < slides.length - 1) goToSlide(currentIndex + 1) });
+btnPrev.addEventListener('click', () => { if (currentIndex > 0) goToSlide(currentIndex - 1) });
+dots.forEach((dot, i) => dot.addEventListener('click', () => goToSlide(i)));
+window.addEventListener('resize', () => goToSlide(0));
+
+goToSlide(0);
